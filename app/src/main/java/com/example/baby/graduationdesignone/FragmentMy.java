1f1bb39840398d4ff609a00f1a9afebc0496652e
xiaopihaier <1428518169@qq.com>
2017-04-15 00:36:55 +0800

    毕业设计

--- app/src/main/java/com/example/baby/graduationdesignone/FragmentMy.java
+++ app/src/main/java/com/example/baby/graduationdesignone/FragmentMy.java
@@ -3,7 +3,6 @@ package com.example.baby.graduationdesignone;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +16,7 @@ import android.widget.LinearLayout;
 public class FragmentMy extends Fragment implements View.OnClickListener {
     ImageView cirImageView;//头像
     LinearLayout about;
+    LinearLayout Cancellation;//注销
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.my, container, false);
@@ -27,6 +27,8 @@ public class FragmentMy extends Fragment implements View.OnClickListener {
     private void IntentView(View v) {
         cirImageView = (ImageView) v.findViewById(R.id.cirImageView);
         cirImageView.setOnClickListener(this);
+        Cancellation = (LinearLayout) v.findViewById(R.id.Cancellation);
+        Cancellation.setOnClickListener(this);
         about = (LinearLayout) v.findViewById(R.id.about);
         about.setOnClickListener(this);
     }
@@ -35,9 +37,11 @@ public class FragmentMy extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.cirImageView:
-                Intent cirImageView = new Intent(getActivity(), Login.class);
-                startActivity(cirImageView);
-                Log.i("my", "my");
+                //启动底部弹出菜单
+                break;
+            case R.id.Cancellation:
+                Intent Cancellation = new Intent(getActivity(), Login.class);
+                startActivity(Cancellation);
                 break;
             case R.id.about:
                 Intent about = new Intent(getActivity(), About.class);
