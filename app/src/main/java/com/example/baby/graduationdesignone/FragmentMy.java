848579e40c332cccd8950f3037c5289052f9e40b
xiaopihaier <1428518169@qq.com>
2017-04-15 10:00:40 +0800

    毕业设计

--- app/src/main/java/com/example/baby/graduationdesignone/FragmentMy.java
+++ app/src/main/java/com/example/baby/graduationdesignone/FragmentMy.java
@@ -3,11 +3,16 @@ package com.example.baby.graduationdesignone;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import static com.example.baby.graduationdesignone.R.id.btn_pick_photo;
+import static com.example.baby.graduationdesignone.R.id.btn_take_photo;
 
 /**
  * Created by baby on 2017/4/14.
@@ -17,6 +22,8 @@ public class FragmentMy extends Fragment implements View.OnClickListener {
     ImageView cirImageView;//头像
     LinearLayout about;
     LinearLayout Cancellation;//注销
+    //自定义的弹出框类
+    PopupMenu menuWindow;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.my, container, false);
@@ -38,6 +45,10 @@ public class FragmentMy extends Fragment implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.cirImageView:
                 //启动底部弹出菜单
+                //实例化SelectPicPopupWindow
+                menuWindow = new PopupMenu(getActivity(), itemsOnClick);
+                //显示窗口
+                menuWindow.showAtLocation(getActivity().findViewById(R.id.my), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0); //设置layout在PopupWindow中显示的位置
                 break;
             case R.id.Cancellation:
                 Intent Cancellation = new Intent(getActivity(), Login.class);
@@ -48,5 +59,36 @@ public class FragmentMy extends Fragment implements View.OnClickListener {
                 startActivity(about);
                 break;
         }
+    }
+
+    //为弹出窗口实现监听类
+    private View.OnClickListener itemsOnClick = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            menuWindow.dismiss();
+            switch (v.getId()) {
+                case btn_take_photo:
+                    btn_take_photo();
+                    Toast.makeText(getActivity(), "拍照", Toast.LENGTH_SHORT).show();
+                    break;
+                case btn_pick_photo:
+                    btn_pick_photo();
+                    Toast.makeText(getActivity(), "相册", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+    };
+
+    private void btn_take_photo() {
+
+    }
+
+    private void btn_pick_photo() {
+
     }
 }
